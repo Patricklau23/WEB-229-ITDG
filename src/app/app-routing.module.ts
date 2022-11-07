28bf4df94d42d44606cb82ed9bf07e69a8a5afff
Pak Tak Lau <plau43@my.centennialcollege.ca>
2022-11-07 14:49:01 -0500

    seesee

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+//import all the pages componments for routings
+import { HomeComponent } from './pages/home/home.component';
+import { SurveyListComponent } from './pages/survey-list/survey-list.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+//set up pathing for routings -> go up to header (nav bar) href change the path e.g /home
+const routes: Routes = [
+{path:'home',component:HomeComponent},
+{path:'survey',component:SurveyListComponent},
+{path:'login',component:LoginComponent},
+{path:'signup',component: SignupComponent},
+{path:'contact',component: ContactComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SurveyListComponent } from './pages/survey-list/survey-list.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SurveyListComponent,
+    LoginComponent,
+    SignupComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
