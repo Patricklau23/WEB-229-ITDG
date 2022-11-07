b19ec37d518675928fa7b06550d201648be83e32
Pak Tak Lau <plau43@my.centennialcollege.ca>
2022-11-07 15:15:07 -0500

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
 {path:'login',component:LoginComponent},
 {path:'signup',component: SignupComponent},
 {path:'contact',component: ContactComponent},
+{path: '', redirectTo: '/home',pathMatch:'full'},
 
 ];
 
